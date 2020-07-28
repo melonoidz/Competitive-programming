52a98dba7b554094d4263c5dd8cfe2212f5354cf
melonoidz <melonoidxyz@gmail.com>
2020-07-29 00:13:04 +0900

    0728

---
+++ practice/beginner/zentansaku/8.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define INF ((1<<30)-1)
+#define LINF (1LL<<60)
+#define EPS (1e-10)
+typedef long long Int;
+typedef pair<Int, Int> P;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int a,b,c,x,y;
+    cin>>a>>b>>c>>x>>y;
+    Int ans=LINF;
+    for(int i=0; i<=2*max(x,y); i++){
+        Int tmp=i*c+max(0,(x-i/2))*a+max(0,(y-i/2)*b);
+        ans=min(ans,tmp);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
