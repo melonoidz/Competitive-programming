ca8999a90b26426fbc665f52643b7da219d2227b
melonoidz <kentaro-318@outlook.jp>
2020-08-16 00:05:12 +0900

    0816

---
+++ practice/middle/dp/daruma.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define INF ((1<<30)-1)
+#define LINF (1LL<<60)
+#define EPS (1e-10)
+typedef long long Int;
+typedef pair<Int, Int> P;
+typedef pair<int, int> pint;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    while(true){
+        int n; cin>>n;
+        if(n==0) break;
+        vector<int> w(n);
+        rep(i,n) cin>>w[i];
+        //両端を取りはずせるか，ダメかで分類
+    }
+    
+    return 0;
+}
