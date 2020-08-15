1ba7e93cb0134ea0ed0a4f755f05caf59939dbe0
melonoidz <kentaro-318@outlook.jp>
2020-08-16 01:20:51 +0900

    0816

---
+++ atcoder/abc175/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define INF ((1<<30)-1)
+#define LINF (1LL<<60)
+#define EPS (1e-10)
+typedef long long Int;
+typedef pair<Int, Int> P;
+typedef pair<int, int> pint;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    string s; cin>>s;
+    int  ans=1;
+    if(s=="SSS") ans=0;
+    else if (s=="SSR") ans=1;
+    else if (s=="SRR") ans=2;
+    else if(s=="RRR") ans=3;
+    else if (s=="RRS") ans=2;
+    else if(s=="RSS") ans=1;
+    cout<<ans<<endl;
+    return 0;
+}
