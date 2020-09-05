9257039f21d895fc5553016443b35843ffcd4c7d
melonoidz <kentaro-318@outlook.jp>
2020-09-05 14:44:13 +0900

    0905

--- atcoder/abc177/e.cpp
+++ atcoder/abc177/e.cpp
@@ -10,13 +10,40 @@ typedef long long Int;
 typedef pair<Int, Int> P;
 typedef pair<int, int> pint;
 struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+
+vector<pair<Int, Int> > prime_factorize(long long N) {
+    vector<pair<long long, long long> > res;
+    for (long long a = 2; a * a <= N; ++a) {
+        if (N % a != 0) continue;
+        long long ex = 0; // 指数
+
+        // 割れる限り割り続ける
+        while (N % a == 0) {
+            ++ex;
+            N /= a;
+        }
+
+        // その結果を push
+        res.push_back({a, ex});
+    }
+
+    // 最後に残った数について
+    if (N != 1) res.push_back({N, 1});
+    return res;
+}
+
+
 int main()
 {
     cin.tie(0);
     ios::sync_with_stdio(false);
     int n; cin>>n;
+    vector<int> pp; 
     vector<int> a(n,0);
-    rep(i,n) cin>>a[i];
-    
+    rep(i,n) 
+    {
+        cin>>a[i];
+        
+    }
     return 0;
 }
