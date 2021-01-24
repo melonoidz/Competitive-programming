e1c6767b601dd9ed05d30ae5e4740d4878012101
melonoidz <melonoidxyz@gmail.com>
2021-01-24 13:32:49 +0900

    0124

--- yukicoder/278/b.cpp
+++ yukicoder/278/b.cpp
@@ -22,62 +22,23 @@ bool ispow2(int i) { return i && (i & -i) == i; }
 ll mask(int i) { return (ll(1) << i) - 1; }
 int lcm(int a, int b) { return a / __gcd(a, b) * b; }
 
-// auto mod int
-// https://youtu.be/L8grWxBlIZ4?t=9858
-// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
-// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
-const int mod = 1000000007;
-struct mint {
-    int x;
-    mint(int x = 0) : x((x % mod + mod) % mod) {}
-    mint operator-() const { return mint(-x); }
-    mint& operator+=(const mint a) {
-        if ((x += a.x) >= mod) x -= mod;
-        return *this;
-    }
-    mint& operator-=(const mint a) {
-        if ((x += mod - a.x) >= mod) x -= mod;
-        return *this;
-    }
-    mint& operator*=(const mint a) {
-        (x *= a.x) %= mod;
-        return *this;
-    }
-    mint operator+(const mint a) const { return mint(*this) += a; }
-    mint operator-(const mint a) const { return mint(*this) -= a; }
-    mint operator*(const mint a) const { return mint(*this) *= a; }
-    mint pow(int t) const {
-        if (!t) return 1;
-        mint a = pow(t >> 1);
-        a *= a;
-        if (t & 1) a *= *this;
-        return a;
-    }
-    // for prime mod
-    mint inv() const { return pow(mod - 2); }
-    mint& operator/=(const mint a) { return *this *= a.inv(); }
-    mint operator/(const mint a) const { return mint(*this) /= a; }
-};
-istream& operator>>(istream& is, const mint& a) { return is >> a.x; }
-ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
-
 signed main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
     cout << fixed << setprecision(20);
+    const int MOD = 1e9 + 7;
     int t;
     cin >> t;
-    rep(p, t) {
+    for (int i = 0; i < t; i++) {
         int n;
         cin >> n;
-        mint tmp(1);
-        rep(i, n) {
-            mint a;
+        int tmp = 1;
+        for (int j = 0; j < n; j++) {
+            int a;
             cin >> a;
-            tmp *= (a + mint(1));
+            tmp *= (a + 1);
+            tmp %= MOD;
         }
-        tmp -= mint(1);
-        cout << tmp << endl;
+        cout << tmp - 1 << endl;
     }
-    return 0;
 }
